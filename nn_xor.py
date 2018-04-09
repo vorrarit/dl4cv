8e443d9f71981bd6df26e0e5d57d6ff57a06e83b
Vorrarit L <vorrarit@gmail.com>
2018-04-09 07:48:15 +0700

    first commit

---
+++ nn_xor.py
@@ -0,0 +1,15 @@
+from pyimagesearch.nn import neuralnetwork
+import numpy as np
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+print("[INFO] training neuralnetwork...")
+nn = neuralnetwork.NeuralNetwork([2, 2, 1], alpha=0.5)
+nn.fit(X, y, epochs=20000)
+
+print("[INFO] testing neuralnetwork...")
+for(x, target) in zip(X, y):
+	pred = nn.predict(x)[0][0]
+	step = 1 if pred > 0.5 else 0
+	print("[INFO] data={}, ground-truth={}, pred={:.4f}, step={}".format(x, target[0], pred, step))
